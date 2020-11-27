57bd6e05feb5114212f5fe91bb7674fddc4c3854
unknown <unknown@example.com>
2020-11-27 22:18:51 +0800

    least

--- pages/flowerroom/flowerroom.js
+++ pages/flowerroom/flowerroom.js
@@ -1,4 +1,7 @@
 // pages/flowerroom/flowerroom.js
+import request from '../../service/network.js'
+const app = getApp()
+const time = require("../../utils/util.js");
 Page({
 
   /**
@@ -6,17 +9,75 @@ Page({
    */
   data: {
     showLog:false,
+    soilMoisture:'',
+    soilFertility:'',
+    flowerLevel:'',
+    mesList:[]
+  },
+  fertilizer(){
+    request({
+      data:{
+        "dormitoryId":app.globalData.dormitoryId,
+        "userId":app.globalData.userId
+      },
+      url: 'balcony/fertilize',
+    }).then(res => {
+      console.log(res)
+      this.onLoad()
+    }).catch(err => { 
+    })
+  },
+  water(){
+    request({
+      data:{
+        "dormitoryId":app.globalData.dormitoryId,
+        "userId":app.globalData.userId
+      },
+      url: 'balcony/watering',
+    }).then(res => {
+      console.log(res)
+      this.onLoad()
+    }).catch(err => {
+    })
   },
   changeLog(){
     this.setData({
       showLog:!this.data.showLog
+    })
+    request({
+      data:{
+        "dormitoryId":app.globalData.dormitoryId
+      },
+      url: 'balcony/plantingRecord',
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        mesList:res.data.records
+      })
+    }).catch(err => {
+      
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(app.globalData.dormitoryId)
+    request({
+      data:{
+        "dormitoryId":app.globalData.dormitoryId
+      },
+      url: 'balcony/getFlowerInf',
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        soilMoisture:res.data.soilMoisture,
+        soilFertility:res.data.soilFertility,
+        flowerLevel:res.data.flowerLevel,
+      })
+    }).catch(err => {
+      
+    })
   },
 
   /**
